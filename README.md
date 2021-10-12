82523b46c873105ea67ad3e98784a8b605cb6d45
hinanshisuthar <hinanshis4@gmail.com>
2021-10-12 11:29:27 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-319uhr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-319uhr)
---
+++ index.js
@@ -0,0 +1,12 @@
+// Import stylesheets
+import './style.css';
+
+var inputText = document.querySelector('#input-text');
+var countBtn = document.querySelector('#count-btn');
+
+countBtn.addEventListener('click', function () {
+  var stringText = inputText.value;
+  // console.log(stringText);
+  var count = stringText.split(' ').length;
+  console.log(count);
+});
